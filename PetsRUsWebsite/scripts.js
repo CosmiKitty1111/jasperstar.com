b7d08ee74704cdcb19fffc5a06ff360417785de2
Jasper Star <jasper.star@outlook.com>
2024-07-06 19:43:50 +1000

    Added the footer template to all of the pages and added some error handling.

--- PetsRUsWebsite/scripts.js
+++ PetsRUsWebsite/scripts.js
@@ -1,15 +1,23 @@
-void async function () {
-    //get the imported document in templates:
-    var templates = document.createElement( 'template' );
-    templates.innerHTML = await ( await fetch( 'templates.html' ) ).text();
+void (async function () {
+  //get the imported document in templates:
+  var templates = document.createElement("template");
+  templates.innerHTML = await (await fetch("templates.html")).text();
 
-    //fetch header and footer templates:
-    var header = templates.content.querySelector( '#header' );
-    var footer = templates.content.querySelector( '#footer' );
-    var headerClone = header.content.cloneNode(true);
-    var footerClone = footer.content.cloneNode(true);
+  //fetch header and footer templates:
+  var header = templates.content.querySelector("#header");
+  var footer = templates.content.querySelector("#footer");
+  var headerClone = header.content.cloneNode(true);
+  var footerClone = footer.content.cloneNode(true);
 
-    //append the templates onto the page:
+  //append the templates onto the page:
+  try {
     document.getElementById("pageHeader").appendChild(headerClone);
+  } catch (e) {
+    console.error("Cannot append header template.\n" + e.message);
+  }
+  try {
     document.getElementById("pageFooter").appendChild(footerClone);
-} ()
+} catch (e) {
+    console.error("Cannot append footer template.\n" + e.message);
+  }
+})();
